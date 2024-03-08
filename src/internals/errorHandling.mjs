053c0a3ac64e939a172623e2726bdd11f8ebd8d1
Nicolas DUBIEN <github@dubien.org>
2024-03-08 09:07:01 +0100

    Better error reporting (including cause)

--- src/internals/errorHandling.mjs
+++ src/internals/errorHandling.mjs
@@ -3,11 +3,11 @@ import process from 'process';
 import { log } from './logger.mjs';
 
 process.on('unhandledRejection', (reason, p) => {
-  log('Unhandled Rejection at: ' + p + ', reason: ' + reason, [], 'error');
+  log(['Unhandled Rejection at:', p], [['reason:', reason]], 'error');
   process.exit(1);
 });
 process.on('uncaughtException', (error) => {
-  log(`Caught exception: ${error}`, [], 'error');
+  log(['Caught exception:', error], [], 'error');
   log(`Exception origin: ${error.stack}`, [], 'error');
   process.exit(1);
 });
--- src/internals/logger.mjs
+++ src/internals/logger.mjs
@@ -1,17 +1,35 @@
+// @ts-check
 const prefixes = { debug: '🐞 ', info: 'ℹ️ ', warn: '⚠️ ', error: '💥 ' };
 
 /**
- * @param {string} message
- * @param {string[]} details
- * @param {keyof typeof console} [kind]
- * @returns {{appendLine: (line:string) => void}}
+ * @param {string|[string, ...unknown[]]} message
+ * @param {(string|[string, ...unknown[]])[]} details
+ * @param {'log'|'debug'|'info'|'warn'|'error'} [kind]
+ * @returns {{appendLine: (line:string|[string, ...unknown[]]) => void}}
  */
 export function log(message, details, kind = 'log') {
   const logger = console[kind ?? 'log'];
   const prefix = prefixes[kind] ?? '';
-  logger(prefix + message);
-  for (const line of details) {
-    logger(`  -> ${line}`);
+
+  if (typeof message === 'string') {
+    logger(prefix + message);
+  } else {
+    logger(prefix + message[0], ...message.slice(1));
   }
-  return { appendLine: (line) => logger(`  -> ${line}`) };
+
+  /**
+   * @param {string|[string, ...unknown[]]} line
+   */
+  const appendLine = (line) => {
+    if (typeof line === 'string') {
+      logger(`  -> ${line}`);
+    } else {
+      logger(`  -> ${line[0]}`, ...line.slice(1));
+    }
+  };
+
+  for (const line of details) {
+    appendLine(line);
+  }
+  return { appendLine };
 }
