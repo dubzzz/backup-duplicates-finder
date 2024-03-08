f413f5dff2721a9ddca989c48c513df65ee5d062
Nicolas DUBIEN <github@dubien.org>
2024-03-08 01:20:14 +0100

    Early exit on unhandled errors

--- src/internals/errorHandling.mjs
+++ src/internals/errorHandling.mjs
@@ -4,8 +4,10 @@ import { log } from './logger.mjs';
 
 process.on('unhandledRejection', (reason, p) => {
   log('Unhandled Rejection at: ' + p + ', reason: ' + reason, [], 'error');
+  process.exit(1);
 });
 process.on('uncaughtException', (error) => {
   log(`Caught exception: ${error}`, [], 'error');
   log(`Exception origin: ${error.stack}`, [], 'error');
+  process.exit(1);
 });
