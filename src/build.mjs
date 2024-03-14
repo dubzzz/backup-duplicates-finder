2e26a11782fb20e9ea554d054eb2fca8c5f9653c
Nicolas DUBIEN <github@dubien.org>
2024-03-14 10:02:14 +0100

    Add depth build

--- src/build.mjs
+++ src/build.mjs
@@ -1,4 +1,6 @@
 // @ts-check
+import path from 'path';
+import fs from 'fs/promises';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import './internals/errorHandling.mjs';
@@ -20,13 +22,35 @@ const argv = await yargs(hideBin(process.argv))
     type: 'boolean',
     description: 'Ignore failures occuring when traversing the tree of files',
   })
+  .option('dig', {
+    default: 0,
+    type: 'number',
+    description: 'Scan leaves at depth {dig} first',
+  })
   .demandCommand(1)
   .parse();
 
+const dig = argv['dig'] ?? 0;
 const isIncremental = !!argv['incremental'];
 const continueOnFailure = argv['fail'] !== false;
 const paths = argv._;
 
+const options = { isIncremental, continueOnFailure, withHash: true };
+
+async function deeper(depth, pathToBeScanned) {
+  if (depth <= 0) {
+    await cachedScanDirectory(String(pathToBeScanned), options);
+    return;
+  }
+  const files = await fs.readdir(pathToBeScanned, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      await deeper(depth - 1, path.join(pathToBeScanned, file.name));
+    }
+  }
+  await cachedScanDirectory(String(pathToBeScanned), options);
+}
+
 for (const path of paths) {
-  await cachedScanDirectory(String(path), { isIncremental, continueOnFailure, withHash: true });
+  await deeper(dig, String(path));
 }
